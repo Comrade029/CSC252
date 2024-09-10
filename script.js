4201083e0578473ed4754a6f219f7654231bb260
comerade029 <okoyepeterchinaza@gmail.com>
2024-09-10 19:19:37 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+            //  1a 
+
+let countries= [];
+let bettercountries=["Canada","England","USA","Dubai"];
+console.log(bettercountries);
+
+        //   1b
+
+let StudentNames=[
+    ["Jude","Chidera","Peter","Divine"],
+    ["Obinna","Ifechukwu","Chinonso","Amaka"],
+    ["ThankGod","Chisom","John","Ada"],
+];
+console.log(StudentNames);
+
+
+        // 1c
+
+let cars=["Benz","Lexus","Hilux"];
+const favcars= cars.join(",");
+console.log(favcars);
+
+
+        // 1d
+
+let soup= [];
+const sweetsoup=["Egusi","Nsala","Oha","Onugwu"];
+for (let index = 0; index < sweetsoup.length; index++) {
+    console.log(sweetsoup[index]);
+    
+}
